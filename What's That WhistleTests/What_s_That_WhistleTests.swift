3025ab9dc34f1dd77977be60f4c809ea031bd39f
Tom Bredemeier <tbredemeier@barrington220.org>
2017-10-06 11:58:25 -0500

    Initial Commit

---
+++ What's That WhistleTests/What_s_That_WhistleTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_s_That_WhistleTests.swift
+//  What's That WhistleTests
+//
+//  Created by tbredemeier on 10/6/17.
+//  Copyright © 2017 tbredemeier. All rights reserved.
+//
+
+import XCTest
+@testable import What_s_That_Whistle
+
+class What_s_That_WhistleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
